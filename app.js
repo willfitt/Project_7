e330d09fe907b4cfec61edd41a934b3cc550ce3c
Will <wmfitt@Hotmail.com>
2019-08-09 22:54:01 -0600

    Revert "project_7"
    This reverts commit 9f5bdff089964ae19cad914b4cdd09ea37f0eff4.

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const db = require('./query');
+const mongoose = require('mongoose');
+const port = process.env.PORT || 3000;
+mongoose.connect('mongodb://localhost/userList',
+    { useNewUrlParser: true });
+const db = mongoose.connection;
+mongoose.set('useFindAndModify', false);
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    console.log('db connected');
+});
 
-const port = process.env.PORT || 5432;
+const userSchema = new mongoose.Schema({
+    fName: String,
+    lName: String,
+    email: String,
+    age: Number,
+});
 
-app.use(express.json);
+const user = mongoose.model('userCollection', userSchema);
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(express.urlencoded({ extended: false }));
@@ -13,27 +28,63 @@ app.use(express.urlencoded({ extended: false }));
 app.get('/', (req, res) => {
     res.render('createUser')
 });
-
-app.get('/userList', db.getUsers);
-
-app.get('/user/edit/:id', db.getUserById);
-
-app.post('/createUser', db.createUser, (req, res) => {
-    res.redirect('/userList')
+app.get('/userList', (req, res) => {
+     mongoose.model('userCollection').find( function (err, users) {
+        if (err) return handleError(err);
+        res.render('userListing', {
+            userArray: users
+        })
+      })    
 });
 
-// app.post('/sortUser', db.sortUser); implement?
-
-// app.post('/findUsers', db.findUsers, (req, res) => {
-//         res.render('userListing');
-// });  implement?
-
-app.post('/editUser/:id', db.updateUser, (req, res) => {
-    res.redirect('/userList')
+app.get('/user/edit/:id', (req, res) => {
+    mongoose.model('userCollection').findById(req.params.id, function(err, user) {
+        if (err) return handleError(err);
+        res.render('editUser', { editUser: user })
+    })
 });
 
-app.post('/deleteUser/:id', db.deleteUser, (req, res) => {
-    res.redirect('/userList')
+app.post('/createUser', (req, res) => {
+    const newUser = new user();
+    newUser.fName = req.body.fName
+    newUser.lName = req.body.lName 
+    newUser.email = req.body.email
+    newUser.age = req.body.age
+    newUser.save((err, data) => {
+        if (err) {
+            return console.error(err);
+        }
+        console.log(`new user save: ${data}`);
+        res.redirect('/userList')
+    });
+});
+
+app.post('/sortUser', (req, res) => {
+    mongoose.model('userCollection').sort()
+});
+
+app.post('/findUsers', (req, res) => {
+    mongoose.model('userCollection').findOne( {fName: req.params.fName}, function(err) {
+        if (err) return handleError(err);
+        res.render('userListing', {
+            userArray: users
+        })
+    })
+})
+
+app.post('/editUser/:id', (req, res) => {
+    mongoose.model('userCollection').findByIdAndUpdate(
+        req.params.id, { fName: req.body.fName, lName: req.body.lName, age: req.body.age, email: req.body.email }, function (err){
+            if (err) return handleError(err);
+            res.redirect('/userList')
+        });
+});
+
+app.post('/deleteUser/:id', (req, res) => {
+    mongoose.model('userCollection').deleteOne({ _id: req.params.id}, function (err){
+        if (err) return handleError(err);
+        res.redirect('/userList')
+      });
 });
 
 
